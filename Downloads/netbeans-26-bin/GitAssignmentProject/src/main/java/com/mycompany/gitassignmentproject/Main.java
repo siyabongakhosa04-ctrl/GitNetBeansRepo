a7eff4b494e8f46bc25b270c894a7dee3f71655d
siyabonga <222509120@ump.ac.za>
2026-04-30 22:28:41 +0200

    Initial commit: Added Hello NetBeans Java file

---
+++ Downloads/netbeans-26-bin/GitAssignmentProject/src/main/java/com/mycompany/gitassignmentproject/Main.java
@@ -0,0 +1,7 @@
+package com.mycompany.gitassignmentproject;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello NetBeans. Welcome to version control using Git");
+    }
+}
