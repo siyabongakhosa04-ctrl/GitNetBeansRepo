ea61d650c419695f350d8fb4444592f11e103696
siyabongakhosa04-ctrl <siyabongakhosa04@gmail.com>
2026-05-01 11:25:50 +0200

    Update Login.java

--- Downloads/netbeans-26-bin/GitAssignmentProject/src/main/java/Login.java
+++ Downloads/netbeans-26-bin/GitAssignmentProject/src/main/java/Login.java
@@ -1,5 +1,5 @@
 public class Login {
     public static void main(String[] args) {
-       System.out.println("Change from GitHub");
+    System.out.println("Local change from NetBeans");;
     }
 }
